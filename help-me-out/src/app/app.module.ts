7fc25fa6140bf31c47a9d29146992dfb8e0bb512
adnan <arronnno@gmail.com>
2021-09-24 23:01:58 -0400

    frontend sceleton created

--- help-me-out/src/app/app.module.ts
+++ help-me-out/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './allComponents/header/header.component';
+import { FooterComponent } from './allComponents/footer/footer.component';
+import { MainComponent } from './allComponents/main/main.component';
+import { NavComponent } from './allComponents/header/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
