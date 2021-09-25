fe0177b0dd1c44d7ba6a4416e83065960cfe9ecd
Tahmidul Alam <31861624+AlamTahmidul@users.noreply.github.com>
2021-09-25 10:10:11 -0400

    clean-app-routing-module

--- help-me-out/src/app/app-routing.module.ts
+++ help-me-out/src/app/app-routing.module.ts
@@ -3,11 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from './allComponents/main/landing/landing.component';
 import { MainComponent } from './allComponents/main/main.component';
 import { AppComponent } from './app.component';
-import { 
-  AuthGuardService as AuthGuard 
-} from './services/auth/auth-guard.service'
-const routes: Routes = [{path: '', component: MainComponent},
-{path: 'home', component: LandingComponent, canActivate:[AuthGuard] },
+import { AuthGuardService as AuthGuard } from './services/auth/auth-guard.service'
+
+const routes: Routes = [
+  {path: '', component: MainComponent},
+  {path: 'home', component: LandingComponent, canActivate:[AuthGuard] }
 ];
 
 @NgModule({
